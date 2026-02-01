71babd62ee117fa27d1d479e37d0da08b9cbb0ff
tirefire <84106878+tire-fire@users.noreply.github.com>
2026-02-01 09:38:16 +0000

    Add scoring models to wipe_competition command

--- web/core/management/commands/wipe_competition.py
+++ web/core/management/commands/wipe_competition.py
@@ -1,6 +1,18 @@
 """Wipe all competition data (nuclear option)."""
 
 from django.core.management.base import BaseCommand, CommandParser
+from scoring.models import (
+    BlackTeamAdjustment,
+    EventScore,
+    FinalScore,
+    IncidentReport,
+    IncidentScreenshot,
+    InjectGrade,
+    OrangeTeamBonus,
+    RedTeamFinding,
+    RedTeamScreenshot,
+    ServiceScore,
+)
 
 from core.models import AuditLog, BotState, DashboardUpdate, DiscordTask
 from team.models import DiscordLink, LinkAttempt, LinkToken, Team
@@ -28,10 +40,23 @@ class Command(BaseCommand):
 
         # Delete in order to avoid foreign key constraints
         counts = {
+            # Ticketing
             "TicketAttachment": TicketAttachment.objects.all().delete()[0],
             "TicketComment": TicketComment.objects.all().delete()[0],
             "TicketHistory": TicketHistory.objects.all().delete()[0],
             "Ticket": Ticket.objects.all().delete()[0],
+            # Scoring (delete children first)
+            "RedTeamScreenshot": RedTeamScreenshot.objects.all().delete()[0],
+            "IncidentScreenshot": IncidentScreenshot.objects.all().delete()[0],
+            "IncidentReport": IncidentReport.objects.all().delete()[0],
+            "RedTeamFinding": RedTeamFinding.objects.all().delete()[0],
+            "InjectGrade": InjectGrade.objects.all().delete()[0],
+            "OrangeTeamBonus": OrangeTeamBonus.objects.all().delete()[0],
+            "ServiceScore": ServiceScore.objects.all().delete()[0],
+            "BlackTeamAdjustment": BlackTeamAdjustment.objects.all().delete()[0],
+            "FinalScore": FinalScore.objects.all().delete()[0],
+            "EventScore": EventScore.objects.all().delete()[0],
+            # Core/Discord
             "DiscordTask": DiscordTask.objects.all().delete()[0],
             "LinkAttempt": LinkAttempt.objects.all().delete()[0],
             "LinkToken": LinkToken.objects.all().delete()[0],
